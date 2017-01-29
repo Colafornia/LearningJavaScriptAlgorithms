06914cd2fb46f1c81e9f374585b5cdafb82bfa3b
MechanicianW <wangyilinchn@foxmail.com>
2017-01-29 21:08:14 +0800

    add DoublyLinkedList

--- dataStructures/DoublyLinkedList.js
+++ dataStructures/DoublyLinkedList.js
@@ -1,22 +1,28 @@
-function LinkedList () {
+function DoublyLinkedList () {
   var Node = function (element) {
     this.element = element
+    this.prev = null
     this.next = null
   }
   var length = 0
   var head = null
+  var end = null
 
   this.append = function (element) {
     var node = new Node(element)
-    var current
+    var current = head
+    var previous
     if (!head) {
       head = node
+      end = node
     } else {
-      current = head
       while (current.next) {
+        previous = current
         current = current.next
       }
       current.next = node
+      node.prev = current
+      end = node
     }
     length++
   }
@@ -28,8 +34,19 @@ function LinkedList () {
       var previous
       var index = 0
       if (position === 0) {
-        node.next = current
-        head = node
+        if (!head) {
+          head = node
+          end = node
+        } else {
+          current.prev = node
+          node.next = prev
+          head = node
+        }
+      } else if (position === length) {
+        current = end
+        current.next = node
+        end = node
+        end.prev = current
       } else {
         while (index < position) {
           previous = current
@@ -37,7 +54,9 @@ function LinkedList () {
           index++
         }
         previous.next = node
+        node.prev = previous
         node.next = current
+        current.prev = node
       }
       length++
       return true
@@ -48,67 +67,34 @@ function LinkedList () {
 
   this.removeAt = function (position) {
     if (position > -1 && position < length) {
-       var current = head
-       var previous
-       var index = 0
-       if (position === 0) {
-         head = current.next
-       } else {
+      var current = head
+      var previous
+      var index = 0
+      if (position === 0) {
+        head = current.next
+        if (!head) {
+          end = head
+        } else {
+          head.prev = null
+        }
+      } else if (position === length) {
+        current = end
+        previous = current.prev
+        previous.next = null
+        end = previous
+      } else {
         while (index < position) {
           previous = current
           current = current.next
           index++
         }
         previous.next = current.next
-       }
-       length--
-       return current.element
-    } else {
-      return null
-    }
-  }
-
-  this.indexOf = function (element) {
-    var current = head
-    var index = 0
-    while (current) {
-      if (current.element === element) {
-        return index
+        current.next.prev = previous
       }
-      index++
-      current = current.next
-    }
-    return -1
-  }
-
-  this.remove = function (element) {
-    var eleIndex = this.indexOf(element)
-    if (eleIndex !== -1) {
-      this.removeAt(eleIndex)
+      length--
+      return current.element
     } else {
       return null
     }
-  }
-
-  this.isEmpty = function () {
-    return length === 0
-  }
-
-  this.size = function () {
-    return length
-  }
-
-  this.getHead = function () {
-    return head
-  }
-
-  this.toString = function () {
-    var string = ''
-    var current = head
-    while (curret) {
-      string = string + current.element
-      current = current.next
-    }
-    return string
   }
 }
