dc1fbd17d2a34fe60d027f1ba1f77ecc5d76f0b8
Colafornia <wangyilinchn@foxmail.com>
2020-03-02 18:32:12 +0800

    Use IIFE to wrap the class

--- DataStructures/HashTable.js
+++ DataStructures/HashTable.js
@@ -1,61 +1,66 @@
-const table = Symbol('table')
-const resize = Symbol('resize')
-class HashTable {
-    constructor (len) {
-        this[table] = new Array(len || 10)
-        this.count = 0
-    }
-
-    hashStringToInt(str, tableSize) {
-        let hash = 17
-        for (let i = 0; i< str.length; i++) {
-            // charCodeAt(): the UTF-16 code unit
-            hash = (hash * str.charCodeAt(i)) % tableSize
+const HashTable = (function () {
+    const table = Symbol('table')
+    const resize = Symbol('resize')
+    class HashTable {
+        constructor(len) {
+            this[table] = new Array(len || 10)
+            this.count = 0
         }
-        return hash
-    }
 
-    [resize]() {
-        const newTable = new Array(this[table].length * 2)
-        this[table].forEach(array => {
-            array && array.forEach(([key, value]) => {
-                this.insertItem(key, value, newTable)
-            })
-        })
-        this[table] = newTable
-    }
-    insertItem(key, value, table) {
-        const idx = this.hashStringToInt(key, table.length)
-        if (table[idx]) {
-            const list = table[idx]
-            const node = list.find(([k, v]) => k === key)
-            if (node) {
-                node[1] = value
-            } else {
-                list.push([key, value])
+        hashStringToInt(str, tableSize) {
+            let hash = 17
+            for (let i = 0; i < str.length; i++) {
+                // charCodeAt(): the UTF-16 code unit
+                hash = (hash * str.charCodeAt(i)) % tableSize
             }
-        } else {
-            table[idx] = [[key, value]]
+            return hash
         }
-    }
 
-    setItem(key, value){
-        this.count++
-        const loadFactor = this.count / this[table].length
-        if (loadFactor > 0.8) {
-            this[resize]()
+        [resize]() {
+            const newTable = new Array(this[table].length * 2)
+            this[table].forEach(array => {
+                array && array.forEach(([key, value]) => {
+                    this.insertItem(key, value, newTable)
+                })
+            })
+            this[table] = newTable
         }
-        this.insertItem(key, value, this[table])
-    }
-    getItem(key){
-        const idx = this.hashStringToInt(key, this[table].length)
-        if (!this[table][idx]) {
-            return null
+        insertItem(key, value, table) {
+            const idx = this.hashStringToInt(key, table.length)
+            if (table[idx]) {
+                const list = table[idx]
+                const node = list.find(([k, v]) => k === key)
+                if (node) {
+                    node[1] = value
+                } else {
+                    list.push([key, value])
+                }
+            } else {
+                table[idx] = [
+                    [key, value]
+                ]
+            }
         }
-        const node = this[table][idx].find(([k, v]) => k === key)
-        return node && node.length ? node[1] : null
+
+        setItem(key, value) {
+            this.count++
+            const loadFactor = this.count / this[table].length
+            if (loadFactor > 0.8) {
+                this[resize]()
+            }
+            this.insertItem(key, value, this[table])
+        }
+        getItem(key) {
+            const idx = this.hashStringToInt(key, this[table].length)
+            if (!this[table][idx]) {
+                return null
+            }
+            const node = this[table][idx].find(([k, v]) => k === key)
+            return node && node.length ? node[1] : null
+        }
     }
-}
+    return HashTable
+})()
 
 const myTable = new HashTable(3)
 myTable.setItem('firstName', 'lala')
